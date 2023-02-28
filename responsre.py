28828e23ba70b3aa80bcdd1ac8ec7299689f167c
KARNATAKAHACKER <125344205+KARNATAKAHACKER@users.noreply.github.com>
2023-03-01 01:02:43 +0530

    Update responsre.py

--- responsre.py
+++ responsre.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-photo3 = "https://telegra.ph/file/485d5b950411e554ff696.jpg"
+photo3 = "https://telegra.ph/file/2c21645cd1c337d12918d.jpg"
 
 def sample_response(input_text):
   user_message = str(input_text).lower()
@@ -9,8 +9,8 @@ def sample_response(input_text):
     return f"{photo3}\n\
     hey! adil is not here i am lara wanna play with me"
 
-  if user_message in ("cute", "baccha", "your crush", "@ID_SELLER00"):
-    return "HE is busy in her schedule. you can tell me i inform her"
+  if user_message in ("agora", "karnataka king", "Agora Bhai", "@Mr_Agora"):
+    return "HE is busy in hid schedule. you can tell me i inform him"
   if user_message in ("bsdk", "madharchod", "bkl", "behanchod", "cgutiya",
                       "gaandu"):
     return "RANDI OFF HAI TO APNI MAA CHUDWA RHA RANDI KA PILA"
